77176cd576d59a571c284902794bb246008d8460
Kamo Petrosyan <kamo@haikson.com>
2015-01-14 01:57:10 +0600

    No critical changes

--- virtenviro/shop/currency_cbr.py
+++ virtenviro/shop/currency_cbr.py
@@ -16,7 +16,8 @@ def update_currency():
     f = open(xml_file, 'wb')
     opener = urllib2.build_opener()
     opener.addheaders = [('User-Agent',
-                          'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/34.0.1847.137 Safari/537.36')]
+                          'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36'
+                          ' (KHTML, like Gecko) Chrome/34.0.1847.137 Safari/537.36')]
     response = opener.open(url)
     data = response.read()
     opener.close()
@@ -44,3 +45,4 @@ def update_currency():
         currency.value = float(valute.find('Value').text)
         currency.nominal = float(valute.find('Nominal').text)
         currency.save()
+
