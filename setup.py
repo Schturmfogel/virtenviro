5720a0ee0174c20564bcc2ab6fe1f19f1cbe0b2b
Kamo Petrosyan <kamo@haikson.com>
2015-04-18 23:26:07 +0600

    setup.py changed

--- setup.py
+++ setup.py
@@ -1,12 +1,32 @@
 from distutils.core import setup
+from setuptools import find_packages, setup
+
+EXCLUDE_FROM_PACKAGES = []
 
 setup(
     name='virtenviro',
     version='0.5.1b',
-    packages=['virtenviro'],
+    packages=find_packages(exclude=EXCLUDE_FROM_PACKAGES),
+    include_package_data=True,
     url='https://github.com/Haikson/virtenviro',
     license='GPL3',
     author='Kamo Petrosyan',
     author_email='kamo@haikson.com',
-    description='Open source content management system (CMS) based on the django framework.'
+    description='Open source content management system (CMS) based on the django framework.',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Internet :: WWW/HTTP :: WSGI',
+        'Topic :: Software Development :: Libraries :: CMS',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
 )
