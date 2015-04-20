7016304313874d03ae1237f631f6968c17b92bed
Kamo Petrosyan <kamo@haikson.com>
2015-04-20 18:05:02 +0600

    fix registration model

--- virtenviro/registration/models.py
+++ virtenviro/registration/models.py
@@ -1,18 +1,19 @@
 #~*~ coding: utf-8 ~*~
 from django.db import models
 from django.contrib.auth.models import User
+from django.conf import settings
+from django.utils.translation import ugettext_lazy as _
 
 
-class UserProfile( models.Model ):
-    user = models.ForeignKey( User, null = True )
-    birthday = models.DateField( blank = True, null = True )
-    language = models.CharField( max_length = 2, choices = LANGUAGES, default = 'ru' )
-    created_at = models.DateTimeField( auto_now_add = True )
-    updated_at = models.DateTimeField( auto_now = True )
+class UserProfile(models.Model):
+    user = models.ForeignKey(User, null=True)
+    birthday = models.DateField(blank=True, null=True)
+    language = models.CharField( max_length=10, choices=settings.LANGUAGES, default='ru')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name=_('Created at'))
+    updated_at = models.DateTimeField(auto_now=True, verbose_name=_('Updated at'))
 
 
 def user_post_save(sender, instance, **kwargs):
-    ( profile, new ) = UserProfile.objects.get_or_create(user=instance)
-
+    (profile, new) = UserProfile.objects.get_or_create(user=instance)
 
 models.signals.post_save.connect(user_post_save, sender=User)
