c5df26d52b1f88544f928d339bbd25cef5213c10
Kamo Petrosyan <kamo@haikson.com>
2015-05-08 15:08:23 +0600

    migration_fixes

--- virtenviro/shop/migrations/0005_product_unique_code.py
+++ virtenviro/shop/migrations/0005_product_unique_code.py
@@ -7,13 +7,13 @@ from django.db import models, migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('shop', '0004_product_unique_code'),
+        ('shop', '0001_initial'),
     ]
 
     operations = [
         migrations.AddField(
             model_name='product',
             name='unique_code',
-            field=models.CharField(unique=True, max_length=250, verbose_name='Unique code', blank=True),
+            field=models.CharField(unique=True, max_length=250, verbose_name='Unique code', blank=True, unique=True),
         ),
     ]
