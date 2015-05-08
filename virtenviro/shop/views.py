8af66d384e1374e464f5abb2bb0068e66bcb0d7d
Kamo Petrosyan <kamo@haikson.com>
2015-05-08 22:31:56 +0600

    try encode str to utf-8

--- virtenviro/shop/views.py
+++ virtenviro/shop/views.py
@@ -76,7 +76,7 @@ def xml_import(tree):
             xml_articul = id_generator(15)
 
         unique_code_string = '%s%s%s' % (xml_name, xml_manufacturer, xml_articul)
-        unique_code = sha256(unique_code_string.decode('utf-8'))
+        unique_code = sha256(unique_code_string.encode('utf-8'))
 
         if not xml_manufacturer == '':
             manufacturer, created = Manufacturer.objects.get_or_create(name=xml_manufacturer)
