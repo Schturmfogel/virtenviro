13211604a0c21276843976de8751f50aa84a3359
Kamo Petrosyan <kamo@haikson.com>
2015-05-10 12:39:41 +0600

    logger open log file with codecs

--- virtenviro/logger.py
+++ virtenviro/logger.py
@@ -1,6 +1,7 @@
 # ~*~ coding: utf-8 ~*~
 __author__ = 'Kamo Petrosyan'
 import os
+import codecs
 from django.conf import settings
 
 MEDIA_ROOT = getattr(settings, 'MEDIA_ROOT', getattr(settings, 'STATIC_ROOT'))
@@ -8,6 +9,6 @@ MEDIA_ROOT = getattr(settings, 'MEDIA_ROOT', getattr(settings, 'STATIC_ROOT'))
 
 class Logger:
     def __init__(self, filename, src):
-        file = open(os.path.join(MEDIA_ROOT, 'logs', filename), 'a')
+        file = codecs.open(os.path.join(MEDIA_ROOT, 'logs', filename), 'utf-8', 'a')
         file.write(src)
         file.close()
